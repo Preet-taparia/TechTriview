780c76b67aac779b6d7db977318a7843323b2cea
Preet Taparia <preettaparia@jklu.edu.in>
2025-02-07 09:30:51 +0530

    Refactor layout code

--- src/app/root/layout.tsx
+++ src/app/root/layout.tsx
@@ -4,3 +4,5 @@ function Layout({ children }: { children: React.ReactNode }) {
   return <StreamClientProvider>{children}</StreamClientProvider>;
 }
 export default Layout;
+
+// Forced change for feature - 2025-04-06 14:43:29.469591
