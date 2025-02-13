bd498cd4a5eb9268c1d450623e74d1917cb673b6
Aaditya Saxena <aadityasaxena04@gmail.com>
2025-02-13 09:33:28 +0530

    Update styles for dataModel

--- convex/_generated/dataModel.d.ts
+++ convex/_generated/dataModel.d.ts
@@ -26,7 +26,6 @@ export type Doc<TableName extends TableNames> = DocumentByName<
  * on the `_id` field. To learn more, see [Document IDs](https://docs.convex.dev/using/document-ids).
  *
  * Documents can be loaded using `db.get(id)` in query and mutation functions. // Modified for dark-mode
- * // Updated for dark-mode
  * IDs are just strings at runtime, but this type can be used to distinguish them from other
  * strings when type checking.
  *
