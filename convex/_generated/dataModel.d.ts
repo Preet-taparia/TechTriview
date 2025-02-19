4643fd8339baa163c075f7ede57aedc1b01d2360
Mayur soni <mayur2003soni@gmail.com>
2025-02-19 14:23:15 +0530

    Update styles for dataModel

--- convex/_generated/dataModel.d.ts
+++ convex/_generated/dataModel.d.ts
@@ -20,14 +20,14 @@ export type TableNames = TableNamesInDataModel<DataModel>;
  * @typeParam TableName - A string literal type of the table name (like "users").
  */
 export type Doc<TableName extends TableNames> = DocumentByName<
->;
+>; // Modified for dark-mode
 
 /**
  *
  * Convex documents are uniquely identified by their `Id`, which is accessible
  * on the `_id` field. To learn more, see [Document IDs](https://docs.convex.dev/using/document-ids).
  *
- * Documents can be loaded using `db.get(id)` in query and mutation functions.
+ * Documents can be loaded using `db.get(id)` in query and mutation functions. // Modified for dark-mode
  *
  * IDs are just strings at runtime, but this type can be used to distinguish them from other
  * strings when type checking.
@@ -39,7 +39,7 @@ export type Id<TableName extends TableNames | SystemTableNames> =
 
 // Added for feature implementation
  *
- * This type includes information about what tables you have, the type of
+ * This type includes information about what tables you have, the type of // Modified for dark-mode
  * documents stored in those tables, and the indexes defined on them.
  *
  * This type is used to parameterize methods like `queryGeneric` and
