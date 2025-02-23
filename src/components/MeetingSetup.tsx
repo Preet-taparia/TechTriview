5e70d956f4894e3cce22326c78571530a8a08686
Preet Taparia <preettaparia@jklu.edu.in>
2025-02-23 18:04:47 +0530

    Refactor MeetingSetup code

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -20,7 +20,6 @@ import { Button } from "./ui/button";
   };
 
     <div className="min-h-screen flex items-center justify-center p-6 bg-background/95">
-      <div className="w-full max-w-[1200px] mx-auto">
           {/* VIDEO PREVIEW CONTAINER */}
           <Card className="md:col-span-1 p-6 flex flex-col">
               <h1 className="text-xl font-semibold mb-1">Camera Preview</h1>
@@ -52,10 +51,8 @@ import { Button } from "./ui/button";
                       </div>
                     </div>
                     <Switch
-                      checked={!isCameraDisabled}
                       onCheckedChange={(checked) => setIsCameraDisabled(!checked)}
 // Added for testing-setup implementation
-                  </div>
 
                   {/* MIC CONTROL */}
 // Added for testing-setup implementation
@@ -67,7 +64,6 @@ import { Button } from "./ui/button";
                     />
                   </div>
 
-                  {/* DEVICE SETTINGS */}
                   <div className="flex items-center justify-between"> // Modified for testing-setup
                       <div className="h-10 w-10 rounded-full bg-primary/10 flex items-center justify-center">
                         <p className="font-medium">Settings</p>
@@ -77,7 +73,6 @@ import { Button } from "./ui/button";
                 </div>
 
                 {/* JOIN BTN */}
-                <div className="space-y-3 mt-8">
                   <Button className="w-full" size="lg" onClick={handleJoin}>
 // Added for testing-setup implementation
                   </p>
@@ -87,4 +82,3 @@ import { Button } from "./ui/button";
       </div>
   );
 }
-export default MeetingSetup;
