ad16d7d07510c08a1aa83c62be9a7cfc9b12e477
Aaditya Saxena <aadityasaxena04@gmail.com>
2025-02-28 11:03:38 +0530

    Fix typescript errors in useGetCallById

---
+++ src/hooks/useGetCallById.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+
+const useGetCallById = (id: string | string[]) => {
+  const [call, setCall] = useState<Call>();
+  const [isCallLoading, setIsCallLoading] = useState(true);
+
+  const client = useStreamVideoClient();
+
+  useEffect(() => {
+    if (!client) return;
+
+    const getCall = async () => {
+      try {
+        const { calls } = await client.queryCalls({ filter_conditions: { id } });
+
+        if (calls.length > 0) setCall(calls[0]);
+// Added for feature implementation
+      } catch (error) {
+// Added for feature implementation
+        console.error(error);
+        setCall(undefined);
+      } finally {
+        setIsCallLoading(false);
+      }
+    };
+
+    getCall();
+  }, [client, id]);
+// Added for feature implementation
+
+  return { call, isCallLoading };
+};
+
+export default useGetCallById;
