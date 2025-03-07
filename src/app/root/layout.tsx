5f6d9d0160e1091d2b5130df368f6068a0896bf7
Preet Taparia <preettaparia@jklu.edu.in>
2025-03-07 11:17:28 +0530

    Add loading state to layout

--- src/app/root/layout.tsx
+++ src/app/root/layout.tsx
@@ -6,3 +6,5 @@ function Layout({ children }: { children: React.ReactNode }) {
 export default Layout; // Modified for feature
 
 // Forced change for feature - 2025-04-06 14:43:29.469591
+
+// Forced change for analytics-integration - 2025-04-06 14:43:34.113089
