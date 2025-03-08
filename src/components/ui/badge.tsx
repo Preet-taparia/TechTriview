c8c510ddb13673fe9428165b52a8a66db51d90e0
Aaditya Saxena <aadityasaxena04@gmail.com>
2025-03-08 10:04:04 +0530

    Implement user-management API

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,9 +4,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
+// Added for feature implementation
   "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
+// Added for feature implementation
       variant: {
         default:
           "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
@@ -28,6 +30,7 @@ export interface BadgeProps
     VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
+// Added for feature implementation
   return (
     <div className={cn(badgeVariants({ variant }), className)} {...props} />
   )
