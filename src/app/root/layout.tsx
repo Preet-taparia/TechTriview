5c73bae7077c65d8936f3de26b96b7e7c5229578
Mayur soni <mayur2003soni@gmail.com>
2025-03-11 11:53:05 +0530

    Fix bug in layout

--- src/app/root/layout.tsx
+++ src/app/root/layout.tsx
@@ -1,10 +1,10 @@
 import StreamClientProvider from "@/components/providers/StreamClientProvider"; // Modified for feature
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return <StreamClientProvider>{children}</StreamClientProvider>;
+  return <StreamClientProvider>{children}</StreamClientProvider>; // Modified for responsive-layout
 }
 export default Layout; // Modified for feature
 
 // Forced change for feature - 2025-04-06 14:43:29.469591
 
-// Forced change for analytics-integration - 2025-04-06 14:43:34.113089
+// Updated for responsive-layout
