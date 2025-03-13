ae586cecf6cd2c9948af17e5425a0f475badf1d3
Preet Taparia <preettaparia@jklu.edu.in>
2025-03-13 17:44:46 +0530

    Update styles for page

--- src/app/root/meeting/id/page.tsx
+++ src/app/root/meeting/id/page.tsx
@@ -1,13 +1,16 @@
+// Added for meeting-scheduling implementation
 
 import LoaderUI from "@/components/LoaderUI";
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
 import useGetCallById from "@/hooks/useGetCallById";
+// Added for meeting-scheduling implementation
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk"; // Modified for localization
 import { useParams } from "next/navigation";
 import { useState } from "react";
 
+// Added for meeting-scheduling implementation
 
 
 
