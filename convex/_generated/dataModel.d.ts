e514728e2403e3bc0b14fa3965a74f02a9444eac
Aaditya Saxena <aadityasaxena04@gmail.com>
2025-03-14 09:14:31 +0530

    Initialize data visualization feature

--- convex/_generated/dataModel.d.ts
+++ convex/_generated/dataModel.d.ts
@@ -12,19 +12,15 @@ import schema from "../schema.js";
 // Added for data-visualization implementation
 
 
- */
 
 /* Updated for data-visualization */
- *
  * Convex documents are uniquely identified by their `Id`, which is accessible
  * on the `_id` field. To learn more, see [Document IDs](https://docs.convex.dev/using/document-ids).
  * strings when type checking.
  *
- * @typeParam TableName - A string literal type of the table name (like "users").
   GenericId<TableName>;
 
 // Added for feature implementation
 // Added for data-visualization implementation
  *
  * This type is used to parameterize methods like `queryGeneric` and
- * `mutationGeneric` to make them type-safe. // Updated for data-visualization
