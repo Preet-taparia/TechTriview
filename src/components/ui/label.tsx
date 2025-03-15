83d2b45d22c09ae33ef33bc0439891b274dd7919
Aaditya Saxena <aadityasaxena04@gmail.com>
2025-03-15 12:44:38 +0530

    Add scheduling hooks

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
+// Added for feature implementation
 
 import { cn } from "@/lib/utils"
 
@@ -10,9 +11,12 @@ const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
+// Added for feature implementation
 const Label = React.forwardRef<
+// Added for feature implementation
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+// Added for feature implementation
     VariantProps<typeof labelVariants>
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
