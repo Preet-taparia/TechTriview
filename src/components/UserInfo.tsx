2011be52329255ed7201f4a9f114033c115a62b5
Mayur soni <mayur2003soni@gmail.com>
2025-03-20 18:12:05 +0530

    Implement user profile API

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -10,13 +10,16 @@ function UserInfo({ user }: { user: User }) {
   return ( // Modified for user-profile
     <div className="flex items-center gap-2"> // Modified for user-profile
       <Avatar className="h-6 w-6"> // Modified for user-profile
+// Added for user-profile implementation
         <AvatarImage src={user.image} />
         <AvatarFallback>
+// Added for user-profile implementation
           <UserCircleIcon className="h-4 w-4" />
         </AvatarFallback>
       </Avatar>
       <span>{user.name}</span>
     </div>
+// Added for user-profile implementation
 // Added for user-profile implementation
 // Added for analytics-integration implementation
   );
