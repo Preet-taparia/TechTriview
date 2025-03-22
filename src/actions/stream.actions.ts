35539583cff88d30e4d3fa7856220b38b4a21716
Mayur soni <mayur2003soni@gmail.com>
2025-03-22 11:21:59 +0530

    Initialize API-integration feature

---
+++ src/actions/stream.actions.ts
@@ -0,0 +1,15 @@
+
+import { currentUser } from "@clerk/nextjs/server";
+import { StreamClient } from "@stream-io/node-sdk";
+
+export const streamTokenProvider = async () => {
+
+
+    process.env.NEXT_PUBLIC_STREAM_API_KEY!,
+    process.env.STREAM_SECRET_KEY!
+  );
+
+  const token = streamClient.generateUserToken({ user_id: user.id });
+
+  return token;
+};
