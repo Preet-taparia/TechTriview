2d3c65a60645960be6d01f277ca545e960f7883a
Preet Taparia <preettaparia@jklu.edu.in>
2025-03-26 12:09:12 +0530

    Add loading state to page

--- src/app/root/meeting/id/page.tsx
+++ src/app/root/meeting/id/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 
 import LoaderUI from "@/components/LoaderUI";
 import MeetingRoom from "@/components/MeetingRoom";
@@ -9,25 +8,18 @@ import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import { useParams } from "next/navigation";
 import { useState } from "react";
 
-function MeetingPage() {
-  const { id } = useParams();
-  const { isLoaded } = useUser();
   const { call, isCallLoading } = useGetCallById(id);
 
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   if (!isLoaded || isCallLoading) return <LoaderUI />;
 
-  if (!call) {
     return (
-      <div className="h-screen flex items-center justify-center">
-        <p className="text-2xl font-semibold">Meeting not found</p>
       </div>
     );
   }
 
   return (
-    <StreamCall call={call}>
         {!isSetupComplete ? (
           <MeetingSetup onSetupComplete={() => setIsSetupComplete(true)} />
         )}
