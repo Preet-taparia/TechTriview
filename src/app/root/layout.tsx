580ab3b67ad847ca82d176df77369bd365a5c07a
Mayur soni <mayur2003soni@gmail.com>
2025-03-28 16:27:01 +0530

    Initialize responsive layout feature

--- src/app/root/layout.tsx
+++ src/app/root/layout.tsx
@@ -1,17 +1,11 @@
 // Added for responsive-layout implementation
 // Added for responsive-layout implementation
 import StreamClientProvider from "@/components/providers/StreamClientProvider"; // Modified for feature
-// Added for responsive-layout implementation
 
-// Added for responsive-layout implementation
 function Layout({ children }: function Layout({ children }_updated,
 // Added for responsive-layout implementation
-  return <StreamClientProvider>{children}</StreamClientProvider>; // Modified for responsive-layout
 // Added for responsive-layout implementation
 }
-export default Layout; // Updated for responsive-layout
 
-// Added for responsive-layout implementation
-// Updated for responsive-layout
 
 // Updated for responsive-layout
